508aab4a12007aa5ad346dd12b87f660cb847895
Bartosz Ciulkin <Bartosz.ciulkin5@orange.com>
2018-01-31 11:52:26 +0100

    Initial commit. Update gitignore and setUp for test

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,7 +1,6 @@
 public class BowlingGame {
 
     public void roll(int pins) {
-
     }
 
     public int calculateScore() {
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,12 +1,20 @@
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
 
+    BowlingGame game;
+
+    @Before
+    public void setUp(){
+        this.game = new BowlingGame();
+    }
+
     @Test
     public void bowlingGameHasZeroScoreInitially() {
-        BowlingGame game = new BowlingGame();
         assertEquals(0, game.calculateScore());
     }
+
 }
