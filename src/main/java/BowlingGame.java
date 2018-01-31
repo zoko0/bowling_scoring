a87d970836237c073708c3138c9e46d68e3f0aa1
Bartosz Ciulkin <Bartosz.ciulkin5@orange.com>
2018-01-31 15:49:53 +0100

    Counts correctly after zero or one throw

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,10 +1,52 @@
 public class BowlingGame {
+    private static final int rounds = 10;
+    private static final int maxPins = 10;
+    private int[][] rolledInRound;
+    private int currentRound= 0;
+    private boolean isFirstRollInRound = true;
 
-    public void roll(int pins) {
+    //konstruktor
+    public BowlingGame(){
+        this.rolledInRound = new int[rounds][2];
+    }
+
+    private int mapRollInRoundToArray( boolean whichRollInRound){
+        if (whichRollInRound)
+            return 0;
+        else
+            return 1;
+    }
+
+    public void roll(int pins) throws Exception {
+        //System.out.println(pins);
+
+        if (pins > maxPins)
+            throw new Exception("Too large number of knocked pins");
+        else {
+            if (isFirstRollInRound){
+                //checkIfStrike
+
+                //this.rolledInRound[currentRound][1] = 0;
+            } else{
+                //checkIfSpare
+
+            }
+            rolledInRound[currentRound++][mapRollInRoundToArray(isFirstRollInRound)] = pins;
+        }
     }
 
     public int calculateScore() {
-        return 0;
+        int finalScore = 0;
+
+        for (int i = 0; i < rounds; i++){
+             System.out.println("Rolled in round " + i + " in firts roll is "
+                     + rolledInRound[i][0] + " and " +  rolledInRound[i][1]);
+
+             finalScore += rolledInRound[i][0] + rolledInRound[i][1];
+
+        }
+
+        return finalScore;
     }
 
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -5,16 +5,27 @@ import static org.junit.Assert.assertEquals;
 
 public class BowlingGameTest {
 
-    BowlingGame game;
+    private BowlingGame game;
 
     @Before
     public void setUp(){
         this.game = new BowlingGame();
     }
 
+    @Test(expected = Exception.class)
+    public void rollReturnsException() throws Exception {
+        game.roll(11);
+    }
+
     @Test
-    public void bowlingGameHasZeroScoreInitially() {
+    public void bowlingGameHasZeroScoreInitially() throws Exception {
+        game.roll(0);
         assertEquals(0, game.calculateScore());
     }
 
+    @Test
+    public void bowlingGameScoreAfterOneRoll() throws Exception {
+        game.roll(5);
+        assertEquals(5, game.calculateScore());
+    }
 }
